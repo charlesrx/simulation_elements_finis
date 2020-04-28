a97e9f4effb87e38e3580b8a39bf7af5a5ef5c16
Charles Roux <crouxpertus@gmail.com>
2020-04-28 17:42:24 +0200

    Rearrangement des fonctions

---
+++ init.py
@@ -0,0 +1,40 @@
+#init
+
+#Les packages------------------------------------------------------------
+from sympy import *
+init_printing(use_unicode=True)
+import numpy as np
+from numpy.linalg import solve
+import matplotlib.pyplot as plt
+
+#Renseigner les valeurs propres au matériau-------------------------------------------------
+E=1.18e11
+nu=0.31
+
+#Paramètres de simulation-------------------------------------------------
+#Nombre d'éléments horizontaux chaque élément à une longueur horizontale de 2
+N=10
+#Nombre d'éléments verticaux, la poutre a une hauteur de 2 divisée par le nombre d'éléments
+NN=6
+
+#--------------------------------------------------------------------------------------------------
+
+#Faire les calculs avec ces paramètres - remarque la force appliquée à la poutre
+#est définie dans ce code via le vecteur b
+exec(open('./multiNy.py').read())
+
+#Afficher les vecteurs déplacement
+AfficherDeplacements()
+
+#Afficher le déplacement du contour
+AfficherContour()
+
+
+#Comparer le résultat avec la déformée calculée
+exec(open('./comparaisonDeformee.py').read())
+
+
+#Comparer le résultat avec la déformée calculée à l'aide de sympy continuum mechanics
+exec(open('./comparaison_sympy_continuummechanics.py').read())
+
+
