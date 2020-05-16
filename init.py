9adc78d0f2b49e19f3b2ba66a3fc60bddd0997e3
Charles Roux <crouxpertus@gmail.com>
2020-05-16 12:12:06 +0200

    modificiation des paramètres utilises dans init

--- init.py
+++ init.py
@@ -13,9 +13,9 @@ nu=0.31
 
 #Paramètres de simulation-------------------------------------------------
 #Nombre d'éléments horizontaux pour le calcul
-N=16
+N=30
 #Nombre d'éléments verticaux pour le calcul
-NN=6
+NN=20
 
 #Longueurs horizontale L (longueur) et verticale H (hauteur) de la poutre
 L=10
